ed6fa3be425aaab8dcdfa075dd5b0e5bc3354cbc
sumitkolhe <sumitkolhe@users.noreply.github.com>
2020-11-25 08:54:04 +0000

    deploy: a35941e1b52ff34258b404fb37195fad245d1722

---
+++ component---src-pages-404-tsx-c731004a1c2658715970.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(n,t,e){"use strict";e.r(t);var c=e("q1tI"),u=e("Wbzz");t.default=function(){return Object(c.useEffect)((function(){Object(u.navigate)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c731004a1c2658715970.js.map
