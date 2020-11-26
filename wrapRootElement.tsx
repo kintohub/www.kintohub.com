88cf76856ee81b17e4ea2bca9a4bb1eef8236e54
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-11-26 08:27:11 +0530

    :bug: fixed identification (#8)
    * :bug: fixed identification
    
    * fixed identification

--- wrapRootElement.tsx
+++ wrapRootElement.tsx
@@ -3,10 +3,20 @@ import { MuiThemeProvider } from "@material-ui/core/styles"
 import { ThemeProvider } from "styled-components"
 import { mainTheme } from "./src/theme/index"
 
+const AnaylticWrapper = ({ children }) => {
+  React.useEffect(() => {
+    // https://segment.com/docs/connections/spec/identify/#anonymous-id
+    window.analytics && window.analytics.identify({})
+  }, [])
+  return <>{children}</>
+}
+
 export const wrapRootElement = ({ element }) => {
   return (
-    <MuiThemeProvider theme={mainTheme}>
-      <ThemeProvider theme={mainTheme}>{element}</ThemeProvider>
-    </MuiThemeProvider>
+    <AnaylticWrapper>
+      <MuiThemeProvider theme={mainTheme}>
+        <ThemeProvider theme={mainTheme}>{element}</ThemeProvider>
+      </MuiThemeProvider>
+    </AnaylticWrapper>
   )
 }
